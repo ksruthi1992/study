8b558c0828aedc02e7052414486b3daf94ada50a
Luke Martin <luke_martin45@yahoo.com>
2017-10-30 10:39:21 -0700

    added getData constructor

--- src/modules/getData.php
+++ src/modules/getData.php
@@ -1,3 +1,16 @@
 /* getData.php
  * main class for getting data used by the front end
  * /
+
+ <?php
+ class getData implaments connect{
+	 
+	string $dataQuesry;
+	 
+	getData(string $dataQuery){
+	  this.dataQuery = dataQuery;
+      databaseConnection();	  
+	}
+	
+	
+ }
