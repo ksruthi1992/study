3858c3e3d140ec1bf644ec0eee4104ddb84fa7aa
Luke Martin <luke_martin45@yahoo.com>
2017-11-02 14:01:22 -0700

    connection as a class

--- src/modules/connect.php
+++ src/modules/connect.php
@@ -1,6 +1,42 @@
 <?php
-	//MySQLi
+/*
+ * Do not modify this without talking to me about it first
+ * we need to work AS A TEAM.
+ *
+ * Thanks,
+ *     Luke 916 952 4724
+ *
+ */
+	
+class connection{
+
+  //hostname
+  $host = "teamtux.ddns.net";
+  //username
+  $user = "congressman";
+  //password
+  $pass = "h@llar!d0n@ld";
+  //database
+  $database= "spotfinder_db";
+
+  $link = mysqli_connect($host, $user, $pass, $database);
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
+
+	//Mysqli	
 	//database connection data
+	/*
+	 * 
 	function databaseConnection(bool $testRequest){
 		//hostname
 		$host = "teamtux.ddns.net";
@@ -34,3 +70,6 @@
 
 	//set to false to connect without performing a test
 	$connection=databaseConnection(false);
+
+	 */
+}
