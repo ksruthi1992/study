17e19cbdcaaf84f1bc7a816e73ce7387b05fef71
traviskeri <traviskeri@csus.edu>
2017-11-30 18:49:11 -0800

    now really added campusDataTest last commit only added the test folder

--- tests/campusDataTest.php
+++ tests/campusDataTest.php
@@ -7,4 +7,17 @@ use PHPUnit\Framework\TestCase;
  * @covers campusData
  */
 
-Class campusDataTest extends TestCAse
+Class campusDataTest extends TestCase{
+
+	public function showCampusTest(){
+		$this->assertEquals(showCampus("all"), "");
+	}
+
+	public function showBuildingTest(){
+		$this->assertEquals(showBuilding("all"), "");
+	}
+
+	public function showFloorTest(){
+		$this->assertEquals(showFloor("all"), '');
+	}
+}
