ab1b988edd6ee3a4749cb3e4a0f2c78144e48bb4
Nick Rentschler <ryanrentschler@csus.edu>
2017-12-03 22:26:57 +0000

    added query for inserting data

--- src/modules/dataSubmit.php
+++ src/modules/dataSubmit.php
@@ -9,6 +9,11 @@
 
 		public function insertDataEntry($campus_id, $building_id, $floor_id, $data){
 
+			$query = " INSERT INTO data 
+					   (campus_id, building_id, floor_id, data)
+					   VALUES
+					   ({$campus_id}, {$building_id}, {$floor_id}, {$data})
+					 ";
         }
         
 		
