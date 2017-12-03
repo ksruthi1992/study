411b4d503bd54734deb77d826291c86303017446
Nick Rentschler <ryanrentschler@csus.edu>
2017-12-03 22:28:50 +0000

    added test values for testing insertDataEntry

--- src/modules/dataSubmit.php
+++ src/modules/dataSubmit.php
@@ -9,11 +9,18 @@
 
 		public function insertDataEntry($campus_id, $building_id, $floor_id, $data){
 
+			$campus_id     = 1;
+			$building_id   = 1;
+			$floor_id      = 1;
+			$data          = 1;
+			
 			$query = " INSERT INTO data 
 					   (campus_id, building_id, floor_id, data)
 					   VALUES
 					   ({$campus_id}, {$building_id}, {$floor_id}, {$data})
 					 ";
+			
+			mysqli_query($this->connection->get(), $query);
         }
         
 		
