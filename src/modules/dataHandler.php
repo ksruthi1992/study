9daf55e3699daba6c046d014966cec6198e09cc7
Nick Rentschler <ryanrentschler@csus.edu>
2017-12-03 22:44:39 +0000

    added a var to the handler

--- src/modules/dataHandler.php
+++ src/modules/dataHandler.php
@@ -4,9 +4,13 @@
     require_once('database_connection.php');
     //campus class
     require_once('campusData.php');
+    //data submit class
+    require_once('dataSubmit.php');
     //database constants
     require_once('constants/database.php');
 
-	$connection = new database_connection(HOSTNAME,USERNAME,PASSWORD,DATABASE);
+	$connection  = new database_connection(HOSTNAME,USERNAME,PASSWORD,DATABASE);
 
-	$campus = new campusData($connection);
+    $campus      = new campusData($connection);
+    
+    $insert_obj  = new dataSubmit($connection);
