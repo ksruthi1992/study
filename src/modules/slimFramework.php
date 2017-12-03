4afacdbda6beb0d8b541d63f0f5478a42c4375c7
Edward Prokopik <ejprok@gmail.com>
2017-12-03 01:14:45 +0000

    updated the slimFramework config stuff so that it is setup for debug mode, and added the debug extension

--- src/modules/slimFramework.php
+++ src/modules/slimFramework.php
@@ -9,12 +9,14 @@
     $container['view'] = function ($container) {
         $view = new \Slim\Views\Twig('../templates',[
             // Cache has been disabled
-            'cache' => false
+            'cache' => false,
+            'debug' => true
         ]);
 
         // Instantiate and add Slim specific extension
         $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
         $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
-
+        $view->addExtension(new Twig_Extension_Debug());
+        
         return $view;
     };
