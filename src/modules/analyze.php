4d33c07f5e2fbcbb38cc4b0075e34a63544ac303
Edward Prokopik <ejprok@gmail.com>
2017-12-06 05:14:46 +0000

    analyze class is mostly functional

--- src/modules/analyze.php
+++ src/modules/analyze.php
@@ -30,8 +30,11 @@
         
         public function calculateAverage($campus_id, $building_id, $floor_id, $data) {
 
+
             if (!$data) 
                 return null;
+
+            
 
             $count = 0;
             $total = 0;
@@ -42,19 +45,52 @@
                             $total += $value;
                         
                     }
-            $calculated_value = (($total) / ($count * 3));
+            $calculated_value =  1 - (($total) / ($count * 3));
 
-            $query = " INSERT INTO cached_data
-                       (campus_id, building_id, floor_id, percentage)
-                       VALUES
-                       ({$campus_id}, {$building_id}, {$floor_id}, {$calculated_value})
+            if ($this->checkCache($campus_id, $building_id, $floor_id)) {
+                
+                $uniq_id = $this->checkCache($campus_id, $building_id, $floor_id);
+                $query = " UPDATE cached_data
+                            SET percentage={$calculated_value}
+                            WHERE id={$uniq_id}
+              ";
 
-                     ";
+            } else {
+
+                $query = " INSERT INTO cached_data
+                            ( campus_id, building_id, floor_id, percentage)
+                            VALUES
+                            ({$campus_id}, {$building_id}, {$floor_id}, {$calculated_value})
+
+
+              ";
+
+            }
+
 
             mysqli_query($this->connection->get(), $query);
 
+
         }
+
+        public function checkCache($campus_id, $building_id, $floor_id) {
+
+                $query = " SELECT * FROM cached_data 
+                WHERE  
+                campus_id     = {$campus_id}    AND 
+                building_id   = {$building_id}  AND
+                floor_id      = {$floor_id}     AND
+                HOUR(time)    = HOUR(CURTIME())
+            ";
+
+        $result =  mysqli_query($this->connection->get(), $query);
+        while($row = $result->fetch_assoc())
+            $data_array[]=$row;
+
+        return $data_array[0]["id"];
+ 
+
         
-        
+        }
 
 	}
