10cf016f923595dfbda3d7525dd4dd9ba36ddc97
Luke Martin <luke_martin45@yahoo.com>
2017-12-09 00:05:42 -0800

    updated doxygen

--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,18 +1,21 @@
 var indexSectionsWithContent =
 {
-  0: "acd",
-  1: "acd"
+  0: "acdg",
+  1: "acd",
+  2: "g"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes"
+  1: "classes",
+  2: "functions"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Data Structures"
+  1: "Data Structures",
+  2: "Functions"
 };
 
